1f287b02e730bf86d043fa0a2a946eafc7f7d3db
CRHarding <casey.r.harding@gmail.com>
2018-03-20 08:09:22 -0400

    Figuring out routes

--- client/src/Components/Home.js
+++ client/src/Components/Home.js
@@ -1,51 +1,51 @@
 import React, { Component } from 'react';
 import axios from 'axios';
 
-import Restaurants from './Restaurants'
+import Restaurants from './Restaurants';
 import Header from './subComponents/Header';
 import Footer from './subComponents/Footer';
 
-
 class Home extends Component {
-	constructor() {
-		super();
-		this.state = {
-			apiDataLoaded: false,
-			apiData: null
-		}
-	};
+  constructor() {
+    super();
+    this.state = {
+      apiDataLoaded: false,
+      apiData: null,
+    };
+  }
 
-	componentDidMount() {
-		return axios.get(`/api/restaurant`)
-			.then(restaurants => {
-				console.log('Restaurants ->', restaurants)
-				this.setState({
-					apiDataLoaded: true,
-					apiData: restaurants.data.data
-				})
-			})
-			.catch(err => {
-				console.log('nope :', err)
-			})
-	};
+  componentDidMount() {
+    return axios
+      .get(`/api/restaurant`)
+      .then(restaurants => {
+        console.log('Restaurants ->', restaurants);
+        this.setState({
+          apiDataLoaded: true,
+          apiData: restaurants.data.data,
+        });
+      })
+      .catch(err => {
+        console.log('nope :', err);
+      });
+  }
 
-	mainListing() {
-		if (this.state.apiDataLoaded) {
-			return this.state.apiData.map((el, i) => {
-				return <Restaurants restaurants={el} key={el.id} />
-			})
-		}
-	};
+  mainListing() {
+    if (this.state.apiDataLoaded) {
+      return this.state.apiData.map((el, i) => {
+        return <Restaurants restaurants={el} key={el.id} />;
+      });
+    }
+  }
 
-	render() {
-		return (
-			<div className="main">
-				<Header />
-					{this.state.apiDataLoaded ? this.mainListing() : 'failed to load'}
-				<Footer />
-			</div>
-		)
-	}
-};
+  render() {
+    return (
+      <div className="main">
+        <Header />
+        {this.state.apiDataLoaded ? this.mainListing() : 'failed to load'}
+        <Footer />
+      </div>
+    );
+  }
+}
 
-export default Home
+export default Home;
