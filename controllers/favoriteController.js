4d7fa97f932d9720726b32196d0ed8e3cc8b3d4d
CRHarding <casey.r.harding@gmail.com>
2018-03-20 14:41:35 -0400

    merging!

--- controllers/favoriteController.js
+++ controllers/favoriteController.js
@@ -1,76 +1,87 @@
 const favorites = require('../models/favoritesDB');
 
 module.exports = {
+  //add the user / restaurant to the favorite database
+  updateFavorite(req, res, next) {
+    const user = window.localStory.getItem();
+    console.log('USER IN UPDATEFAVORITE--->', user);
+    const favorite = { user: user, restaurant: req.params.id };
+    favorites.alreadyFavorites(favorite).then(favorite => {
+      favorites
+        .updateFavorites({
+          username: req.body.username,
+          restaurant_name: req.body.restaurantName,
+        })
+        .then(favorite => {
+          next();
+        })
+        .catch(err => {
+          next(err);
+        })
+        .catch(err => {
+          const like = {
+            user: req.session.user.username,
+            restaurant: parseInt(req.params.id),
+          };
+          favorites
+            .removeFavorite(favorites)
+            .then(workLike => {
+              next();
+            })
+            .catch(err => {
+              next(err);
+            });
+        });
+    });
+  },
 
-  /*
-      HAVE TO FIGURE OUT HOW TO STORE CURRENT USER BEFORE
-      IMPLEMENTING FAVORITING
-  */
+  alreadyLikes(req, res, next) {
+    const like = {
+      user: req.session.user.username,
+      restaurant: parseInt(req.params.id),
+    };
+    favorites
+      .alreadyFavorites(like)
+      .then(doesnLike => {
+        res.locals.alreadyLikes = true;
+        next();
+      })
+      .catch(err => {
+        res.locals.alreadyLikes = false;
+        next();
+      });
+  },
 
-//add the user / restaurant to the like database
-  // updateFavorite(req, res, next) {
-  //   if (!res.locals.alreadyLikes) {
-  //     const favorite = { 'writer': res.locals.friendUser, 'friend': req.session.user.username, 'post': parseInt(req.params.id) };
-  //     favorites.updateFavorites({
-  //       username: req.body.username,
-  //       restaurant_name: req.body.restaurantName
-  //     })
-  //     .then(favorite => {
-  //       next();
-  //     })
-  //     .catch(err => {
-  //       next(err);
-  //     });
-  //   } else {
-  //     const like = { 'friend': req.session.user.username, 'post': parseInt(req.params.id) };
-  //     favorites.removeFavorite(favorites)
-  //     .then(workLike => {
-  //       next();
-  //     })
-  //     .catch(err => {
-  //       next(err);
-  //     });
-  //   }
-  // },
-  //
-  // alreadyLikes(req, res, next) {
-  //   const like = { 'friend': req.session.user.username, 'post': parseInt(req.params.id) };
-  //   likes.alreadyLikes(like)
-  //   .then(doesnLike => {
-  //     res.locals.alreadyLikes = true;
-  //     next();
-  //   })
-  //   .catch(err => {
-  //     res.locals.alreadyLikes = false;
-  //     next();
-  //   });
-  // },
-
-  // removeLike(req, res, next) {
-  //   const dislike = { 'friend': req.session.user.username, 'post': parseInt(req.params.id) };
-  //   likes.removeLike(dislike)
-  //   .then(workDislike => {
-  //     next();
-  //   })
-  //   .catch(err => {
-  //     next(err);
-  //   });
-  // },
+  removeLike(req, res, next) {
+    const dislike = {
+      user: req.session.user.username,
+      restaurant: parseInt(req.params.id),
+    };
+    favorites
+      .removeFavorite(dislike)
+      .then(workDislike => {
+        next();
+      })
+      .catch(err => {
+        next(err);
+      });
+  },
 
   getLikes(req, res, next) {
     const restaurantName = req.params.id;
-    likes.getLikes(restaurantName)
-    .then(totalFavorites => {
-      console.log('GETTING TOTAL FAVORITES WORKED--->', totalFavorites);
-      res.json({
-        message: 'ok',
-        data: totalFavorites,
+    favorites
+      .getFavorites(restaurantName)
+      .then(totalFavorites => {
+        console.log('GETTING TOTAL FAVORITES WORKED--->', totalFavorites);
+        res.json({
+          message: 'ok',
+          data: totalFavorites,
+        });
+        next();
+      })
+      .catch(err => {
+        console.log('GETTING TOTAL FAVORITES FAILED--->', err);
+        next(err);
       });
-      next();
-    })
-    .catch(err => {
-      console.log('GETTING TOTAL FAVORITES FAILED--->', err);
-      next(err);
-    });
   },
 };
