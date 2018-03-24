d17c293d10fae0192bd75fccd54ba20fff2f348d
TomChoe <doodeitstom@yahoo.com>
2018-03-24 17:54:24 -0400

    working on main page

--- client/src/Components/RestaurantComponents/RestMap.js
+++ client/src/Components/RestaurantComponents/RestMap.js
@@ -2,8 +2,13 @@ import React, { Component } from 'react';
 
 const RestMap = (props) => {
 	return (
-		<div>
-			<iframe src={`https://www.google.com/maps/embed/v1/search?q=${props.location}&key=AIzaSyCgPb79qOvgUSyoGsPIhQSDnfNl5_DFasM`} allowfullscreen></iframe>
+		<div className="map">
+		<br/>
+			<iframe
+					width={"600"}
+					height={"400"}
+					src={`https://www.google.com/maps/embed/v1/search?q=${props.location}&key=AIzaSyCgPb79qOvgUSyoGsPIhQSDnfNl5_DFasM`} allowfullscreen>
+			</iframe>
 		</div>
 	)
 }
